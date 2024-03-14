d79a06b4d90bd6da21d905d76e8febe9648f207e
LuizOtaviodeSouza <lui-otavio@hotmail.com>
2024-03-14 10:55:58 -0300

    bbb

--- SilvinhaApp/src/components/Form/ResultImc/index.js
+++ SilvinhaApp/src/components/Form/ResultImc/index.js
@@ -1,11 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function ResultImc() {
+export default function ResultImc(props) {
   return (
-    <View>
-      <Text>Resultado</Text>
-      <Text>Resultado do I.M.C</Text>
+    <View style={styles.container}>
+
+      <Text style={styles.text}>
+        {props.messageResult}
+       </Text>
+
+      <Text style={styles.textImc}>
+        {props.ResultImc}
+       </Text>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+ container:{
+   flex: 1,
+   width:"100%",
+   lignItems:"center",
+   justifyContent:"center",
+   marginTop:-150,
+  },
+
+  text:{
+   fontSize:18,
+   color:"#000",
+   fontWeight:"bold"
+  },
+
+  texImc:{
+   fontSize:48,
+   color:"#000",
+   fontWeight:"bold"
+  },
+
+})
